ad91e657256f60bfe030cdc4c5faa7a2edc7cfef
test <lnttam.543@gmail.com>
2022-08-10 23:31:20 +0700

    hcn

---
+++ hcn.py
@@ -0,0 +1,20 @@
+# dung thu vien turtle
+import turtle
+
+# tao man hinh ve
+tam_screen = turtle.Screen()
+
+# tao cay but 
+tam_turtle1 = turtle.Turtle()
+
+#ve hinh chu nhat
+tam_turtle1.speed(1)
+tam_turtle1.forward(50)
+tam_turtle1.right(90)
+tam_turtle1.forward(100)
+tam_turtle1.right(90)
+tam_turtle1.forward(50)
+tam_turtle1.right(90)
+tam_turtle1.forward(100)
+
+tam_turtle1.done()
