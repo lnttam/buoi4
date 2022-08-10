b502393a1c4ecc2ad00f943aaaf1f3c9021320c9
test <lnttam.543@gmail.com>
2022-08-10 23:33:01 +0700

    hinhtron

---
+++ hinhtron.py
@@ -0,0 +1,12 @@
+# dung thu vien turtle
+import turtle
+
+# tao man hinh ve
+tam_screen = turtle.Screen()
+
+# ve hinh tron
+tam_turtle3 = turtle.Turtle()
+tam_turtle3.speed(1)
+tam_turtle3.circle(100)
+
+tam_turtle3.done()
