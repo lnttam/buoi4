4bebce8de0d860f566174295ae51768cd96dd723
test <lnttam.543@gmail.com>
2022-08-10 23:33:51 +0700

    tamgiac

---
+++ tamgiac.py
@@ -0,0 +1,17 @@
+# dung thu vien turtle
+import turtle
+
+# tao man hinh ve
+tam_screen = turtle.Screen()
+
+#ve tam giac
+tam_turtle2 = turtle.Turtle()
+tam_turtle2.speed(1)
+tam_turtle2.right(45)
+tam_turtle2.forward(100)
+tam_turtle2.right(135)
+tam_turtle2.forward(140)
+tam_turtle2.right(135)
+tam_turtle2.forward(100)
+
+tam_turtle2.done()
