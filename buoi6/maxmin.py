a012bd11e1e5d84f8edc6b83f078e073ea8fdd80
test <lnttam.543@gmail.com>
2022-08-25 21:47:32 +0700

    tim max min

---
+++ buoi6/maxmin.py
@@ -0,0 +1,13 @@
+import math
+
+a = int(input("nhap so = "))
+b = int(input("nhap so = "))
+c = int(input("nhap so = "))
+d = int(input("nhap so = "))
+e = int(input("nhap so = "))
+
+lonnhat = max(a,b,c,d,e)
+nhonhat = min(a,b,c,d,e)
+
+print("so lon nhat = ", lonnhat)
+print("so nho nhat = ", nhonhat)
