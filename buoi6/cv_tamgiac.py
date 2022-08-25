bb8f4763f6c3203647f3055477e7808bef6a2072
lnttam <110729836+lnttam@users.noreply.github.com>
2022-08-25 22:24:51 +0700

    Delete buoi6 directory

--- buoi6/cv_tamgiac.py
+++
@@ -1,7 +0,0 @@
-a = int(input("nhap canh 1 = "))
-b = int(input("nhap canh 2 = "))
-c = int(input("nhap canh 3 = "))
-
-chuvi = a+b+c
-
-print("chu vi tam giac = ", chuvi)
--- buoi6/tinhtuoi.py
+++
@@ -1,9 +0,0 @@
-namsinh = int(input("nhap nam sinh = "))
-tuoi = 2022 - namsinh
-print("so tuoi = ", tuoi)
-
-# import math
-# n=30
-# m=4
-# sotrang = math.ceil(n/m)
-# print("so trang = ", sotrang)
